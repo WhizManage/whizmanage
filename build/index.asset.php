72892ea180cd1c26bd2e40abc11b1a890ab86047
JosefElishayov <121160405+JosefElishayov@users.noreply.github.com>
2026-02-01 10:20:08 +0200

    chore: clean up code structure and remove unused code blocks

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '324b36ad6afc6c7091c9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '02656833ab9d14255d5e');
