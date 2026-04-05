fa9f845b9a8537e7090300dd5b5a381def8179c0
JosefElishayov <121160405+JosefElishayov@users.noreply.github.com>
2026-04-05 16:12:53 +0300

    chore: rebuild assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '50ad88313d2bda18965e');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '72b55ff2bbb9b7bc0212');
